85876940fb90f1711f65941db91ad3da1c7fb726
lambda1014 <69205195+lambda1014@users.noreply.github.com>
2020-11-09 18:15:13 +0900

    movie app

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+        Yu Jeong's Movie App ^v^
+    </div>
+  );
+}
+
+export default About;
